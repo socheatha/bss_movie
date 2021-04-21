1e77889d8ade32aab064c2197ce0aebf1e1e17a4
socheatha <teysocheatha@gmail.com>
2021-04-21 22:58:41 +0700

    --

---
+++ detail.php
@@ -0,0 +1,12 @@
+<?php
+    require_once 'autoload.api';
+?>
+
+<?php bss_include_layout('header.layout', [], ['is_front_end' => 1]); ?>
+    <div class="row">
+        <div class="col-md-3">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/08mXo-Xb1kQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+    </div>
+<?php bss_include_layout('footer.layout'); ?>
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hello
+<?php
+    require_once 'autoload.api';
+?>
+
+<?php bss_include_layout('header.layout', [], ['is_front_end' => 1]); ?>
+    <div class="row">
+        <?php foreach (get_movies() as $movie) { ?>
+            <div class="col-md-3"> <?php bss_thumbnail($movie); ?> </div>
+        <?php } ?>
+    </div>
+<?php bss_include_layout('footer.layout'); ?>
